f236fff882051b2b09b2a77294de328e4ee61962
Grzegorz Lisowski <lisux1991@tlen.pl>
2023-11-11 17:56:05 +0100

    Add google site verification
    To make the site easier to find we want to verify it in google.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="EuA5Nuw0rHLwPHet-x4zb3HTKZ4o-kMQUJvSANgUpvQ" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
