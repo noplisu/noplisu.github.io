f0349b712a5039555654ac723fef3bcf69fb4f0f
Grzegorz Lisowski <lisux1991@tlen.pl>
2023-11-12 01:40:38 +0100

    Make contact form response messages better
    The contact form had a very simple thank you message and the error
    messages had no styling. We fixed it by adding a more robust thank you
    message and adding styling.

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -5,56 +5,71 @@ import { useForm, ValidationError } from '@formspree/react';
 
 export default function ContactForm() {
   const [state, handleSubmit] = useForm("xqkvozrr");
-  if (state.succeeded) {
-      return <p>Thanks for joining!</p>;
-  }
   return (
     <section id="contact-form" className="bg-white bg-gray-100 h-[calc(100vh-68px)] min-h-[calc(600px)]">
       <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
-          Ready to elevate your digital presence?
-        </h2>
-        <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
-           {"Fill out the form below and let's kickstart your project into high gear. I'm excited to turn your ideas into robust, user-friendly applications. Let's innovate, code, and create success!"}
-        </p>
-        <form onSubmit={handleSubmit} className="space-y-8">
-          <div>
-            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-              Email Address
-            </label>
-            <input
-              id="email"
-              type="email" 
-              name="email"
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-              placeholder="john.doe@example.com"
-            />
-            <ValidationError 
-              prefix="Email" 
-              field="email"
-              errors={state.errors}
-            />
-          </div>
-          <div className="sm:col-span-2">
-            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">
-              Your message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-              placeholder="Leave a message..."
-            />
-            <ValidationError 
-              prefix="Message" 
-              field="message"
-              errors={state.errors}
-            />
-          </div>
-          <button type="submit" disabled={state.submitting} className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-indigo-500 sm:w-fit hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-primary-300">
-            Submit
-          </button>
-        </form>
+        { state.succeeded && (
+          <>
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+              Thank You for Reaching Out!
+            </h2>
+            <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+              {"Thank you for taking the time to share your project details with me! 🌟 I'm excited about the opportunity to collaborate with you and turn your ideas into reality. I'm already reviewing the information you provided and I'll get back to you shortly. In the meantime, feel free to explore more about my work and services on the website."}
+            </p>
+          </>
+        ) }
+        { !state.succeeded && (
+          <>
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+              Ready to elevate your digital presence?
+            </h2>
+            <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+              {"Fill out the form below and let's kickstart your project into high gear. I'm excited to turn your ideas into robust, user-friendly applications. Let's innovate, code, and create success!"}
+            </p>
+            <form onSubmit={handleSubmit} className="space-y-8">
+              <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                  Email Address
+                </label>
+                <input
+                  id="email"
+                  type="email"
+                  name="email"
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                  placeholder="john.doe@example.com"
+                />
+                <div className="text-red-700 text-xs px-2 py-1 rounded relative" role="alert">
+                  <ValidationError
+                    prefix="Email"
+                    field="email"
+                    errors={state.errors}
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">
+                  Your message
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Leave a message..."
+                />
+                <div className="text-red-700 text-xs px-2 py-1 rounded relative" role="alert">
+                  <ValidationError
+                    prefix="Message"
+                    field="message"
+                    errors={state.errors}
+                  />
+                </div>
+              </div>
+              <button type="submit" disabled={state.submitting} className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-indigo-500 sm:w-fit hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-primary-300">
+                Submit
+              </button>
+            </form>
+          </>
+        ) }
       </div>
     </section>
   );
