1a3f496cab4c39d2432e6b98dbe676fba2aa47fd
Grzegorz Lisowski <lisux1991@tlen.pl>
2023-11-16 03:11:14 +0100

    Correct meta description
    The meta description I was using was too long. It is better for the
    description to be between 150-220 characters.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Grzegorz Lisowski Portfolio',
-  description: "Seasoned developer with a decade of expertise (since 2013) in full stack development, specializing in the exciting realms of AR/VR applications. From crafting robust back-end solutions to creating immersive virtual experiences, explore my portfolio to witness a decade of innovation and proficiency. Elevate your projects with a developer who combines experience, creativity, and a passion for cutting-edge technology. Let's transform ideas into extraordinary digital realities together.",
+  description: "Seasoned developer with a decade of expertise (since 2013) in full stack development. Crafting robust solutions and creating immersive virtual experiences.",
 }
 
 export default function RootLayout({
